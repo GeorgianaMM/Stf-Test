6ba9172441c40774f03c32efb8a9e60c652b1124
Georgiana <georgiana.marti.gg@gmail.com>
2023-11-26 16:36:44 +0200

    test animation

--- Stf Test/Assets/IdleTutorialGame.cs
+++ Stf Test/Assets/IdleTutorialGame.cs
@@ -5,19 +5,48 @@ public class IdleTutorialGame : MonoBehaviour
     public Text coinsText;
     public double coins;
 
+
+    public Text coinsPerSecText;
+    public Text clickUpgrade1Text;
+    public Text productionUpgrade1Text;
+
+
+    public double coinsPerSecond;
+    public double clickUpgrade1Cost; //Cost of upgrade power-up
+    public int clickUpgrade1Level;   //Level of upgrade power-up
+    public double clickUpgrade1Power; //Power of upgrade power-up
+
+
+    public double productionUpgrade1Cost; //Cost of production power-up
+    public int productionUpgrade1Level; ////Level of production power-up
+    //public double productionUpgrade1Power; //Power of production power-up
+
+
     void Start()
     {
         coins = 0;
-    }
+        clickUpgrade1Cost = 10;
+        productionUpgrade1Cost = 25;
 
+    }
     
+
     void Update()
     {
+        coinsPerSecond = productionUpgrade1Level;
+
         coinsText.text = "Coins " + coins;
+        coinsPerSecText.text = coinsPerSecond + " coins/s";
+        clickUpgrade1Text.text = "Click Upgrade 1\nCost: " + clickUpgrade1Cost + " coins\nPower: +1 Click\nLevel: " + clickUpgrade1Level;
+        productionUpgrade1Text.text = "Production Upgrade 1\nCost: " + productionUpgrade1Cost + " coins\nPower: +1 coins/s\nLevel: " + productionUpgrade1Level;
     }
 
+
+    //Buttons
     public void Click()
     {
         coins +=1;
     }
+
+    
 }
