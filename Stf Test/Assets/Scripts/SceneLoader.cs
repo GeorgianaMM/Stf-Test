6281967f1b3f152ffb9f4792e1d8cb87a01bd144
Georgiana <georgiana.marti.gg@gmail.com>
2024-01-04 23:39:20 +0200

    Updated project

--- Stf Test/Assets/Scripts/SceneLoader.cs
+++ Stf Test/Assets/Scripts/SceneLoader.cs
@@ -5,42 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    //public Main main;
-    public void LoadNextScene()
+    public void LoadMainScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        MainUI mainUI = FindObjectOfType<MainUI>();
-        
-        if (mainUI != null)
-        {
-            mainUI.enabled = false;
-        }
-
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        SceneManager.LoadScene("Main");
     }
-
-    public void LoadPreviousScene()
-    {
-        SceneManager.LoadScene(1);
-        SceneManager.sceneLoaded += OnMainSceneLoaded;
-    }
-
-    private void OnMainSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.buildIndex == 1) // Assuming the Main scene is at build index 1
-        {
-            MainUI mainUI = FindObjectOfType<MainUI>();
-            
-            if (mainUI != null)
-            {
-                mainUI.enabled = true;
-                mainUI.UpdateUI();
-            }
-            
-            // Since we've done our job, unsubscribe from the event
-            SceneManager.sceneLoaded -= OnMainSceneLoaded;
-        }
-    }
-
-
 }
